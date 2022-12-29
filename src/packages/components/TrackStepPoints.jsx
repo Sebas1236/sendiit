9b05a5d25bab522120cdbd5aa4a8b8d34dd570b7
Juanh44 <juah7522@gmail.com>
2022-12-29 00:22:27 -0600

    improve step points

--- src/packages/components/TrackStepPoints.jsx
+++ src/packages/components/TrackStepPoints.jsx
@@ -1,18 +1,21 @@
 export const TrackStepsPoints = ({step}) => {
 	const totalSteps = 5;
-	return (
+	if (step < 1)
+		return (<></>)
+	else 
+		return (
 		<div className="d-flex justify-content-center mt-4">
-			<div className="d-inline-flex align-items-center justify-content-center">
+			<div className="d-flex align-items-center justify-content-center">
 				{[...Array(totalSteps)].map((e, i) => (
-					<>
-						<div key={i} className={"d-inline-flex align-items-center justify-content-center circle "
+					<span key={i} className="d-flex align-items-center">
+						<div className={"d-flex align-items-center justify-content-center pe-1 circle "
 							.concat(i+1 < step ? "done " : "")
 							.concat(i+1 === step ?"current " : "")}>
 								{i+1 < step && <i className="fa fa-check check"></i>}
 						</div>
 							{ i+1 < step && <hr className="green-line"></hr>}
 							{ i+1 >= step && i+1 < totalSteps && <hr className="gray-line"></hr>}
-					</>
+					</span>
 				))}
 			</div>
 				 
