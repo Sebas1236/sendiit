51be22b59fb61a9336c2852e6c4f5d1a03c9a9cf
Sebas1236 <cptsebas@gmail.com>
2023-01-01 11:19:04 -0600

    Corregido mensaje al actualizar repartidor

--- src/components/admin/NewUserModal.jsx
+++ src/components/admin/NewUserModal.jsx
@@ -59,7 +59,7 @@ export const NewUserModal = () => {
 
         closeNewUserModal();
         activeUser ?
-            Swal.fire('Éxito, los datos del repartidor han sido actualizados.', 'success')
+            Swal.fire('Éxito', 'Los datos del repartidor han sido actualizados.', 'success')
             :
             Swal.fire('Éxito', 'Se ha registrado al repartidor.El repartidor deberá revisar su correo y crear una nueva contraseña para ingresar al sistema.', 'success');
     }
