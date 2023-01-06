c0b262d4b9f4c2d1c07b5ded7b024da00bc625fd
Sebas1236 <cptsebas@gmail.com>
2023-01-05 22:09:15 -0600

    Solved merge conflicts MenuRepartidor

--- src/components/repartidor/MenuRepartidor.jsx
+++ src/components/repartidor/MenuRepartidor.jsx
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-import { useAuthStore, usePackageStore, usePlacesStore } from '../../hooks'
-import { FooterLanding, Navbar } from '../';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import RouteIcon from '@mui/icons-material/Route';
-import Stack from '@mui/material/Stack';
-import { NavLink } from 'react-router-dom';
-import { useEffect } from 'react';
-
-export const MenuRepartidor = () => {
-
-    const { user } = useAuthStore();
-    // const { startLoadingAllPackages } = usePackageStore();
-    // useEffect(() => {
-    //     startLoadingAllPackages();
-    // }, []);
-    
-    return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <Navbar />
-            </div>
-            <h3>Bienvenido {user.name}</h3>
-            <Stack direction="row" spacing={2}>
-                <NavLink
-                    exact="true"
-                    to="/ver-rutas"
-                    className="nav-link"
-                    children={({ isActive }) => (
-                        isActive
-                            ?
-                            <Button variant="outlined" startIcon={<RouteIcon />}>
-                                Ver rutas
-                            </Button>
-                            :
-                            <Button variant="outlined" startIcon={<RouteIcon />}>
-                                Ver rutas
-                            </Button>
-                    )}
-                />
-                <NavLink
-                    exact="true"
-                    to="/ver-paquetes"
-                    className="nav-link"
-                    children={({ isActive }) => (
-                        isActive
-                            ?
-                            <Button variant="contained" endIcon={<SendIcon />}>
-                                Ver paquetes
-                            </Button>
-                            :
-                            <Button variant="contained" endIcon={<SendIcon />}>
-                                Ver paquetes
-                            </Button>
-                    )}
-                />
-            </Stack>
-            <div className='row'>
-=======
 import { FooterLanding } from '../FooterLanding';
 import { Link } from 'react-router-dom';
 import '../../css/bienvenido.css'
@@ -108,17 +48,70 @@ export const MenuRepartidor = () => {
 
             <div className='row altura2'></div>
             <div className='row mt-5'>
->>>>>>> 5d5737507bc8b586f1c070c89fc7d27ee3cb195b
                 <FooterLanding />
             </div>
 
         </div>
-<<<<<<< HEAD
-    )
-}
-=======
 
 
     )
 }
->>>>>>> 5d5737507bc8b586f1c070c89fc7d27ee3cb195b
+
+// import { useAuthStore, usePackageStore, usePlacesStore } from '../../hooks'
+// import { FooterLanding, Navbar } from '../';
+// import Button from '@mui/material/Button';
+// import SendIcon from '@mui/icons-material/Send';
+// import RouteIcon from '@mui/icons-material/Route';
+// import Stack from '@mui/material/Stack';
+// import { NavLink } from 'react-router-dom';
+// import { useEffect } from 'react';
+
+// export const MenuRepartidor = () => {
+
+//     const { user } = useAuthStore();
+//     // const { startLoadingAllPackages } = usePackageStore();
+//     // useEffect(() => {
+//     //     startLoadingAllPackages();
+//     // }, []);
+    
+//     return (
+//         <div className='container-fluid'>
+//             <div className='row'>
+//                 <Navbar />
+//             </div>
+//             <h3>Bienvenido {user.name}</h3>
+//             <Stack direction="row" spacing={2}>
+//                 <NavLink
+//                     exact="true"
+//                     to="/ver-rutas"
+//                     className="nav-link"
+//                     children={({ isActive }) => (
+//                         isActive
+//                             ?
+//                             <Button variant="outlined" startIcon={<RouteIcon />}>
+//                                 Ver rutas
+//                             </Button>
+//                             :
+//                             <Button variant="outlined" startIcon={<RouteIcon />}>
+//                                 Ver rutas
+//                             </Button>
+//                     )}
+//                 />
+//                 <NavLink
+//                     exact="true"
+//                     to="/ver-paquetes"
+//                     className="nav-link"
+//                     children={({ isActive }) => (
+//                         isActive
+//                             ?
+//                             <Button variant="contained" endIcon={<SendIcon />}>
+//                                 Ver paquetes
+//                             </Button>
+//                             :
+//                             <Button variant="contained" endIcon={<SendIcon />}>
+//                                 Ver paquetes
+//                             </Button>
+//                     )}
+//                 />
+//             </Stack>
+//             <div className='row'></div>
